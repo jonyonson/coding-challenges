524032d0e9ab50931453904c41f2f5abf8e287ba
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-05-10 11:47:57 -0500

    bubble sort

---
+++ hackerrank/bubble-sort.js
@@ -0,0 +1,18 @@
+function countSwaps(a) {
+  let num_swaps = 0;
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < a.length - 1; j++) {
+      // Swap adjacent elements if they are in decreasing order
+      if (a[j] > a[j + 1]) {
+        let temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
+        num_swaps++;
+      }
+    }
+  }
+
+  console.log(`Array is sorted in ${num_swaps} swaps.`);
+  console.log('First Element:', a[0]);
+  console.log('Last Element:', a[a.length - 1]);
+}
