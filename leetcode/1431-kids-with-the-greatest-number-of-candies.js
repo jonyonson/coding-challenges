adb441fe4b46eaeafdb0683142e6747f8b351104
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-05-12 12:10:17 -0500

    1431. Kids With the Greatest Number of Candies

---
+++ leetcode/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function (candies, extraCandies) {
+  let max_candies = Math.max(...candies);
+  let result = [];
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] + extraCandies >= max_candies) result.push(true);
+    else result.push(false);
+  }
+
+  return result;
+};
