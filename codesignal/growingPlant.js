1aa9211a1d085e8a8176ddad4c2407e2aa87d655
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-05-14 11:15:40 -0500

    growingPlant

---
+++ codesignal/growingPlant.js
@@ -0,0 +1,15 @@
+function growingPlant(upSpeed, downSpeed, desiredHeight) {
+  let height = 0;
+  let days = 0;
+  while (height < desiredHeight) {
+    height += upSpeed;
+    if (height >= desiredHeight) {
+      days++;
+      break;
+    }
+    height -= downSpeed;
+    days++;
+  }
+
+  return days;
+}
