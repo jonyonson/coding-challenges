9833e6486f6ff896220c47ad4dc8d643588c1e45
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-05-18 09:49:54 -0500

    insert a node at the tail of a linked list

---
+++ hackerrank/insert-a-node-at-the-tail-of-a-linked-list.js
@@ -0,0 +1,23 @@
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function insertNodeAtTail(head, data) {
+  const new_node = { data, next: null };
+  if (head === null) return new_node;
+  let current_node = head;
+
+  while (current_node !== null) {
+    if (current_node.next === null) {
+      current_node.next = new_node;
+      return head;
+    }
+
+    current_node = current_node.next;
+  }
+}
