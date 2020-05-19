999508f3ca2c7f063af04e43c571ff70d668215b
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-05-19 09:05:21 -0500

    901. Online Stock Span

---
+++ leetcode/0901-online-stock-span.js
@@ -0,0 +1,26 @@
+var StockSpanner = function () {
+  this.prices = [];
+  this.weights = [];
+};
+
+/**
+ * @param {number} price
+ * @return {number}
+ */
+StockSpanner.prototype.next = function (price) {
+  let weight = 1;
+  while (this.prices.length && this.prices[this.prices.length - 1] <= price) {
+    this.prices.pop();
+    weight += this.weights.pop();
+  }
+
+  this.prices.push(price);
+  this.weights.push(weight);
+  return weight;
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = new StockSpanner()
+ * var param_1 = obj.next(price)
+ */
