cf51a1f2c81065cc08aab893daaf3e900522cf80
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-05-25 08:09:37 -0500

    0345. Reverse Vowels of a String

---
+++ leetcode/0345-reverse-vowels-of-a-string.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function (s) {
+  let arr = s.split('');
+  let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
+  if (!vowels.includes(arr)) return arr.join('');
+  let leftIndex = 0;
+  let rightIndex = s.length - 1;
+
+  while (leftIndex < rightIndex) {
+    while (!vowels.includes(arr[leftIndex])) {
+      leftIndex++;
+    }
+
+    while (!vowels.includes(arr[rightIndex])) {
+      rightIndex--;
+    }
+
+    if (leftIndex < rightIndex) {
+      let temp = arr[leftIndex];
+      arr[leftIndex] = arr[rightIndex];
+      arr[rightIndex] = temp;
+    }
+
+    leftIndex++;
+    rightIndex--;
+  }
+
+  return arr.join('');
+};
