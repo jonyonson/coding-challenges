8eaac2eb52aa4a0eb05e0a85b738ee7ddf8f9447
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-02 08:10:45 -0500

    1464. Maximum Product of Two Elements in an Array

---
+++ leetcode/1464-maximum-product-of-two-elements-in-an-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+  let largest = Number.MIN_VALUE;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if ((nums[i] - 1) * (nums[j] - 1) > largest) {
+        largest = (nums[i] - 1) * (nums[j] - 1);
+      }
+    }
+  }
+
+  return largest;
+};
