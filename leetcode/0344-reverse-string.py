8ad60126de247e1abf8b2b437b696cac21f162b4
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-04 08:39:41 -0500

    344. Reverse String (Python)

---
+++ leetcode/0344-reverse-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left_index = 0
+        right_index = len(s) - 1
+
+        while (left_index < right_index):
+            s[left_index], s[right_index] = s[right_index], s[left_index]
+
+            left_index += 1
+            right_index -= 1
