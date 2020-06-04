8c35b1c7e3630f4f5b95665f100704fee11e2830
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-04 08:40:37 -0500

    fix linting errors

--- leetcode/0344-reverse-string.py
+++ leetcode/0344-reverse-string.py
@@ -1,13 +1,12 @@
-class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        left_index = 0
-        right_index = len(s) - 1
+def reverseString(s):
+    """
+    Do not return anything, modify s in-place instead.
+    """
+    left_index = 0
+    right_index = len(s) - 1
 
-        while (left_index < right_index):
-            s[left_index], s[right_index] = s[right_index], s[left_index]
+    while (left_index < right_index):
+        s[left_index], s[right_index] = s[right_index], s[left_index]
 
-            left_index += 1
-            right_index -= 1
+        left_index += 1
+        right_index -= 1
