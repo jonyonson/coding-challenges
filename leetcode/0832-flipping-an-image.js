68a746b35d38459d3139dedefa12d835112eda33
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-20 08:09:15 -0500

    832. Flipping an Image

---
+++ leetcode/0832-flipping-an-image.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+// var flipAndInvertImage = function (A) {
+//   return A.map((row) =>
+//     row.reverse().map((x) => {
+//       if (x === 0) return 1;
+//       else return 0;
+//     }),
+//   );
+// };
+
+const flipAndInvertImage = (A) => A.map((row) => row.reverse().map((x) => 1));
