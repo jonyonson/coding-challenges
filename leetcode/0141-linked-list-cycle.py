b62cae818d10352851d860d88899ca32a355c9bb
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-24 10:00:23 -0500

    141. Linked List Cycle

---
+++ leetcode/0141-linked-list-cycle.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+
+        # seen = set()
+
+        # current = head
+
+        # while (current is not None):
+        #     if current in seen:
+        #         return True
+        #     seen.add(current)
+        #     current = current.next
+
+        # return False
+
+        if head is None or head.next is None:
+            return False
+
+        slow = head
+        fast = head.next
+
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+
+        return True
