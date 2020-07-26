790c308596ff3f4ca57a13bc7360a0bfdb4c3755
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-07-26 13:05:01 -0500

    square digits sequence

---
+++ codesignal/square-digits-sequence.js
@@ -0,0 +1,10 @@
+function squareDigitsSequence(a0) {
+  let count = {};
+  let curr = a0;
+  while (!count[curr]) {
+    count[curr] = true;
+    a = [...curr.toString()].reduce((acc, cv) => acc + +(cv ** 2), 0);
+  }
+
+  return Object.keys(count).length + 1;
+}
